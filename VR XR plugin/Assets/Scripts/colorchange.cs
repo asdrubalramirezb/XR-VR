681b56cfadd183fd472fcef98aafb626b4db8809
asdrubalramirezb <asdrubalramirezb@users.noreply.github.com>
2021-08-01 22:52:36 -0500

    co
    mmit

---
+++ VR XR plugin/Assets/Scripts/colorchange.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class colorchange : MonoBehaviour
+{
+    public Material pinkColor = null;
+    public Material grayColor = null ;
+    private MeshRenderer meshRenderer = null ;
+    private XRGrabInteractable grabInteractable = null;
+
+    private void Awake ()
+    {
+            meshRenderer = GetComponent<MeshRenderer>();
+           // grabInteractable = GetComponent<XRGrabInteractable>();
+
+           // grabInteractable.OnActivate.AddListener(SetPink);
+           // grabInteractable.OnDeactivate.AddListener(SetGray);
+    } 
+
+       private void OnDestroy ()
+    {
+            //grabInteractable.OnActivate.RemoveListener(SetPink);
+            //grabInteractable.OnDeactivate.RemoveListener(SetGray);
+    }
+
+       public void SetPink (/*XRBaseInteractor interactor*/)
+    {
+        meshRenderer.material =  pinkColor;  
+    } 
+    
+       public void SetGray (/*XRBaseInteractor interactor*/)
+    {
+        meshRenderer.material =  grayColor;  
+         
+    } 
+}
+
